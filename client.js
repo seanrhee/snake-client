2ada082a1c73715d80d529463f21c943673a56f6
Sean Lee <lee.sean.cw@gmail.com>
2022-09-04 17:23:53 +0000

    add connect message + send name to game

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 3000
+  })
+
+  conn.on('connect', () => {
+    console.log("Successfully Connected!");
+    conn.write("Name: SCL");
+  });
+  
+
+  
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  conn.on('end', () => {
+    console.log("You have been disconnected.");
+  });
+
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,26 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  }, () => {
-    console.log("Successfully Connected!");
-  })
-  
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  });
-
-  conn.on('end', () => {
-    console.log("You have been disconnected.");
-  });
-
-  conn.setEncoding('utf8');
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log('Connecting...');
 connect();
