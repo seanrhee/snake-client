05b87b6e2becdf876fee1c79137c9dd88ddbbb93
Sean Lee <lee.sean.cw@gmail.com>
2022-09-05 17:18:03 +0000

    add inputs for WASD

--- input.js
+++ input.js
@@ -1,12 +1,28 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
+  connection = conn;
+
   const handleUserInput = function (key) {
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w') {
+      connection.write("Move: up");
+    }
+    if (key === 's') {
+      connection.write("Move: down");
+    }
+    if (key === 'a') {
+      connection.write("Move: left");
+    }
+    if (key === 'd') {
+      connection.write("Move: right");
     }
   }
 
